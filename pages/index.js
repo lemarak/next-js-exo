69e6720907bb30fe5cc00915867d1e456bf5a485
Stéphane Dauguet <sdauguet@gmail.com>
2021-06-17 18:34:46 +0200

    next/link instead next/Link

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/Link";
+import Link from "next/link";
 import ReactMarkdown from "react-markdown";
 import gfm from "remark-gfm";
 import axios from "axios";
